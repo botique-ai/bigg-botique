8425629dc20a95127251aecf89ebb6877ea63adc
Netanel Gilad <netanelgilad@gmail.com>
2016-07-10 10:33:52 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const gulp = require('gulp');
+const path = require('path');
+const install = require('gulp-install');
+const ts = require('gulp-typescript');
+const nodemon = require('gulp-nodemon');
+const http = require('http');
+const gulpFilter = require('gulp-filter');
+const mocha = require('gulp-mocha');
+const sourcemaps = require('gulp-sourcemaps');
+const jeditor = require("gulp-json-editor");
+
+const tsProject = ts.createProject(path.join(__dirname, 'tsconfig.json'), {
+  typescript: require('typescript')
+});
+
+function installDeps() {
+  return gulp.src('package.json')
+    .pipe(jeditor((json) => {
+      if (json.dependencies) {
+        json.dependencies['source-map-support'] = '0.4.0';
+      }
+
+      else {
+        json.dependencies = {'source-map-support': '0.4.0'};
+      }
+      return json;
+    }))
+    .pipe(gulp.dest('.'))
+    .pipe(install({
+      args: '--progress=false'
+    }));
+}
+module.exports.install = gulp.series(installDeps);
+
+function complileTs() {
+  const tsResult = gulp.src([
+    'src/**/*.ts',
+    'typings/**/*.d.ts'
+  ])
+    .pipe(sourcemaps.init())
+    .pipe(ts(tsProject));
+
+
+  return tsResult.js
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('dist/js'));
+}
+module.exports.compile = gulp.series(complileTs);
+
+function runTests() {
+  return gulp.src('./dist/js/**/*')
+    .pipe(gulpFilter(['**/*.spec.js']))
+    .pipe(mocha())
+    .pipe(gulp.dest('../.build/coverage/reports'));
+}
+module.exports.test = gulp.series(installDeps, complileTs, runTests);
+
+function runServer() {
+  gulp.watch('src/**/*', gulp.series(complileTs));
+
+  nodemon({
+    script: 'dist/js/index.js',
+    watch: 'dist/js',
+    delay: 10,
+    ext: 'js',
+    env: {'NODE_ENV': 'development'}
+  });
+}
+module.exports['run-server'] = gulp.series(installDeps, complileTs, runServer);
