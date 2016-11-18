be07f551fc45d6b68e84e2e94c7a40e46561102d
Netanel Gilad <netanelgilad@gmail.com>
2016-11-18 09:21:10 +0200

    add definitions to package

--- biggfile.ts
+++ biggfile.ts
@@ -1,8 +1,14 @@
 import {compileAndWatch} from "bigg-typescript/src/tasks/compileAndWatch";
+import {compile as originalCompile} from "bigg-typescript";
 
 export = {
   biggs: [
     'bigg-typescript',
+    function compile() {
+      return originalCompile({
+        declarations: true
+      });
+    },
     function develop() {
       return compileAndWatch({
         declarations: true
